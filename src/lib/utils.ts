644106cd1743e50b20d68946562b941a764bd2c8
Induktr <nikitavoitenko2020@gmail.com>
2025-02-03 18:10:04 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,35 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { logger } from '@/lib/logger'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null;
+
+  return function executedFunction(...args: Parameters<T>) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(later, wait);
+  };
+}
+
+const loggedMessages = new Set<string>();
+
+export const logOnce = (key: string, message: string, data?: any) => {
+  if (!loggedMessages.has(key)) {
+    console.log(message, data);
+    loggedMessages.add(key);
+  }
+};
